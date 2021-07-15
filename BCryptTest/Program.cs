26ac23c7fe4e229a25bb4b1a388304bdc055c58d
Chengyu Sun <csun@calstatela.edu>
2021-07-15 11:46:15 -0700

    Added BCryptTest example

---
+++ BCryptTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BCryptTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a password: ");
+            var password1 = Console.ReadLine();
+            var hash = BCrypt.Net.BCrypt.HashPassword(password1);
+            Console.WriteLine("Hash: {0}", hash);
+
+            Console.Write("Please enter the password again: ");
+            var password2 = Console.ReadLine();
+            if (BCrypt.Net.BCrypt.Verify(password2, hash))
+                Console.WriteLine("The passwords match.");
+            else
+                Console.WriteLine("The passwords do not match.");
+        }
+    }
+}
